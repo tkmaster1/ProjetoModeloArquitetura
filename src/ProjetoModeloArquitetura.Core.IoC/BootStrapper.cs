d070ac16111471aa28fb51d24355f8637027472f
tkmaster1 <45917644+tkmaster1@users.noreply.github.com>
2021-10-10 17:00:17 -0300

    Add project files.

---
+++ src/ProjetoModeloArquitetura.Core.IoC/BootStrapper.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjetoModeloArquitetura.Core.IoC
+{
+    public class BootStrapper
+    {
+        public static void Register()
+        {
+            // Lifestyle.Transient => Uma instância para cada solicitação
+            // Lifestyle.Singleton => Uma instância única para a classe (para servidor)
+            // Lifestyle.Scoped => Uma instância única para o request
+
+            #region Domain
+
+            #endregion
+
+            #region InfraData
+
+            #endregion
+        }
+    }
+}
---
+++ src/ProjetoModeloArquitetura.Core.Utilities/Util.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace ProjetoModeloArquitetura.Core.Utilities
+{
+    public class Util
+    {
+    }
+}
